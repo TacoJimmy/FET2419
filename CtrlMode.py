9584f2ad563260a94f8c0bd9c18900ae3803c655
TacoJimmy <t5669016@ntut.org.tw>
2022-07-14 16:05:09 +0800

    Update CtrlMode.py

--- CtrlMode.py
+++ CtrlMode.py
@@ -20,6 +20,13 @@ def read_AutoMode():
         ACCtrlMode.close
     return PWdemand['Automode']
 
+def read_CuntAuto():
+    with open('Ctrlmode.json') as ACCtrlMode:
+        PWdemand = json.load(ACCtrlMode)
+        ACCtrlMode.close
+    return PWdemand['Autocunt']
+
+
 def count_automode():
     with open('Ctrlmode.json') as ACCtrlChange:
         ACOPSET = json.load(ACCtrlChange)
